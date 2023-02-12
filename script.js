3450d68b32a4e54690d7d28755c6edfb034912c7
Krsiak Daniel <krsiak.daniel@gmail.com>
2023-02-12 22:32:45 +0100

    counter

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-// scripts
+// === TEXTAREA COUNTER
+const textareaEl = document.querySelector('.form__textarea');
+const counterEl = document.querySelector('.counter');
+
+const inputListener = () => {
+  const textareaMaxChars = 150;
+  const currentTextLength = textareaEl.value.length;
+  const remainingChars =  textareaMaxChars - currentTextLength;
+
+  // update counter value
+  counterEl.textContent = remainingChars === 0 ? "Text limit reached." : remainingChars;
+}
+
+// listen for changes in textarea on event 'input' (when user types)
+textareaEl.addEventListener('input', inputListener);
+
